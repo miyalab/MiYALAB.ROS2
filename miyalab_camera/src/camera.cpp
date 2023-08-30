70cb0e54c7d9d39d90398b29fe98bae1fc85839b
miyalab <miyalab.workspace@gmail.com>
2023-08-30 11:25:52 +0900

    update

--- miyalab_camera/src/camera.cpp
+++ miyalab_camera/src/camera.cpp
@@ -36,8 +36,9 @@ Camera::Camera(rclcpp::NodeOptions options) : rclcpp::Node("camera", options)
     RCLCPP_INFO(this->get_logger(), "Initialize parameters...");
     m_param.device       = this->declare_parameter("camera.device", "/dev/video0");
     m_param.format       = this->declare_parameter("camera.format", "MPEG");
-    m_param.frame_height = this->declare_parameter("camera.frame.width", 1280);
-    m_param.frame_width  = this->declare_parameter("camera.frame.height", 720);
+    m_param.frame_width  = this->declare_parameter("camera.frame.width", 1280);
+    m_param.frame_height = this->declare_parameter("camera.frame.height", 720);
+    m_param.fps          = this->declare_parameter("camera.fps", 30);
     RCLCPP_INFO(this->get_logger(), "Complete! Parameters were initialized.");
 
     // Initialize publisher
@@ -71,14 +72,16 @@ void Camera::run()
     capture.set(cv::CAP_PROP_FRAME_HEIGHT, m_param.frame_height);
     capture.set(cv::CAP_PROP_FRAME_WIDTH, m_param.frame_width);
     capture.set(cv::CAP_PROP_FPS, m_param.fps);
-
+    capture.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc(m_param.format[0], m_param.format[1], m_param.format[2], m_param.format[3]));
+    
     // Main loop
-    for(rclcpp::WallRate loop(1000.0/m_param.fps); rclcpp::ok(); loop.sleep()){
+    for(rclcpp::WallRate loop(m_param.fps); rclcpp::ok(); loop.sleep()){
         auto img_msg = std::make_unique<sensor_msgs::msg::Image>();
         cv_bridge::CvImage cv_img;
 
-        capture >> cv_img.image;
-        cv_img.header.frame_id = "cap";
+        // capture >> cv_img.image;
+        capture.read(cv_img.image);
+        cv_img.header.frame_id = m_param.device;
         cv_img.header.stamp = this->now();
         cv_img.encoding = "bgr8";
 
@@ -86,7 +89,7 @@ void Camera::run()
         m_image_publisher->publish(std::move(img_msg));
 
         cv::imshow("frame", cv_img.image);
-        cv::waitKey(1);
+        if(cv::waitKey(1) == 'q') break;
     }
 
     // カメラ開放
