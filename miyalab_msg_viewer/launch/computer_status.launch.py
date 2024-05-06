6b83029376c1358db12074f2155b7d3d50cb310b
miyalab <miyalab.workspace@gmail.com>
2024-05-06 19:14:04 +0900

    add viewer launch file

---
+++ miyalab_msg_viewer/launch/computer_status.launch.py
@@ -0,0 +1,39 @@
+from inspect import Parameter
+from os.path import join
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_prefix = get_package_share_directory('miyalab_msg_viewer')
+   
+    container = Node(
+	    package='rclcpp_components',
+		executable='component_container',
+		name = 'viewer_container',
+		emulate_tty = True,
+		output = 'screen'
+	)
+
+    components = LoadComposableNodes(
+        target_container="viewer_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package='miyalab_msg_viewer',
+                plugin='MiYALAB::ROS2::ComputerStatusViewer',
+                name='viewer',
+                remappings=[
+                    # subscriber
+                    ("/computer/status", "/computer/status"),
+                ],
+                extra_arguments=[{'use_intra_process_comms': True}]
+            )
+        ]
+
+    )
+    return LaunchDescription([
+        container,
+        components
+    ])
